66240b7c735bf65181a96b2bf019dd9d3cfb2b0b
Weremir <weremir1997@o2.pl>
2020-05-05 13:50:12 +0200

    Task 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -3,8 +3,56 @@ package com.company;
 import java.io.File;
 
 public class Animal {
-     String species;
-     Double weight;
+     final String species;
+     private Double weight;
      String name;
      File pic;
+     static final Double DEFAULT_DOG_WEIGHT = 6.0;
+     static final Double DEFAULT_BEAR_WEIGHT = 260.0;
+     static final Double DEFAULT_SNAKE_WEIGHT = 2.0;
+
+     public Animal(String species){
+          this.species = species;
+          switch (species){
+               case "dog":
+                    this.weight = DEFAULT_DOG_WEIGHT;
+                    break;
+               case "bear":
+                    this.weight = DEFAULT_BEAR_WEIGHT;
+                    break;
+               case "snake":
+                    this.weight = DEFAULT_SNAKE_WEIGHT;
+                    break;
+          }
+     }
+
+
+     void feed() {
+          if (weight > 0) {
+               weight++;
+               System.out.println("Nice thx for food, after that my weight is " + weight);
+
+          } else {
+               System.out.println("too late, sorry");
+          }
+     }
+
+     void takeForAWalk() {
+          weight--;
+          if (weight <= 0) {
+               System.out.println("walking with a dead animal is probably not normal and you can't do that");
+
+          } else if (weight < 3) {
+               System.out.println("Walk was nice but now I am hungry");
+
+          } else {
+               System.out.println("Thanks for taking me for a walk");
+          }
+
+
+     }
 }
+
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Animal dog = new Animal("dog");
+        dog.name = "Pug";
+
+        Human me = new Human();
+        me.firstName = "Michał";
+        me.lastName = "Cięszczyk-Jaworski";
+
+        me.pet = dog;
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+
 
     }
 }
